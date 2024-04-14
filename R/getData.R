c7418d709ebd9ce2c6ae6e2f0a6259363b369095
Tylar <murray.tylar@gmail.com>
2024-04-14 13:42:17 -0400

    switch getData to use cleaned & + getRawData fn

--- R/getData.R
+++ R/getData.R
@@ -1,8 +1,17 @@
 # loads the data and converts the column but also keeps a copy of 
 # the original (_orig) column
-getData <- function(){
+getRawData <- function(){
   data <- readr::read_csv(
     here::here("data/Unified_WQ_Database_1995-2022.csv"),
+    show_col_types = FALSE)
+  data$Value_orig <- data$Value
+  data$Value <- as.numeric(data$Value)
+  return(data)
+}
+
+getData <- function(){
+  data <- readr::read_csv(
+    here::here("data/df_cleaned.csv"),
     show_col_types = FALSE)
   data$Value_orig <- data$Value
   data$Value <- as.numeric(data$Value)
